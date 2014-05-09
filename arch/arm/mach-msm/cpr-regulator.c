db34cae7a1cef690898ee6ba32e1c873e350e9b8
Andrew Dodd <atd7@cornell.edu>
2014-05-08 20:48:15 -0400

    Partially revert "msm: cpr-regulator: remove register save and restore for suspend and resume"
    This partially reverts commit d11db4359ae58f963010493e69370702a27c129d.
    
    Restore register save/load but keep the mutex locking for race condition fixes
    
    Conflicts:
    	arch/arm/mach-msm/cpr-regulator.c
    
    Change-Id: I6586c63c1d25f1735697924bd02345533d30b4e9

--- arch/arm/mach-msm/cpr-regulator.c
+++ arch/arm/mach-msm/cpr-regulator.c
@@ -119,6 +119,7 @@
 #define CPR_INT_DEFAULT	(CPR_INT_UP | CPR_INT_DOWN)
 
 #define CPR_NUM_RING_OSC	8
+#define CPR_NUM_SAVE_REGS	10
 
 /* RBCPR Clock Control Register */
 #define RBCPR_CLK_SEL_MASK	BIT(0)
@@ -204,6 +205,9 @@ struct cpr_regulator {
 	int		*save_ctl;
 	int		*save_irq;
 
+	u32		save_regs[CPR_NUM_SAVE_REGS];
+	u32		save_reg_val[CPR_NUM_SAVE_REGS];
+
 	/* Config parameters */
 	bool		enable;
 	u32		ref_clk_khz;
@@ -223,7 +227,7 @@ struct cpr_regulator {
 	u32		num_corners;
 	int		*quot_adjust;
 
-	bool		is_cpr_suspended;
+	bool            is_cpr_suspended;
 };
 
 #define CPR_DEBUG_MASK_IRQ	BIT(0)
@@ -404,6 +408,28 @@ static bool cpr_ctl_is_busy(struct cpr_regulator *cpr_vreg)
 
 	reg_val = cpr_read(cpr_vreg, REG_RBCPR_RESULT_0);
 	return reg_val & RBCPR_RESULT0_BUSY_MASK;
+}
+
+static void cpr_regs_save(struct cpr_regulator *cpr_vreg)
+{
+	int i, offset;
+
+	for (i = 0; i < CPR_NUM_SAVE_REGS; i++) {
+		offset = cpr_vreg->save_regs[i];
+		cpr_vreg->save_reg_val[i] = cpr_read(cpr_vreg, offset);
+	}
+}
+
+static void cpr_regs_restore(struct cpr_regulator *cpr_vreg)
+{
+	int i, offset;
+	u32 val;
+
+	for (i = 0; i < CPR_NUM_SAVE_REGS; i++) {
+		offset = cpr_vreg->save_regs[i];
+		val = cpr_vreg->save_reg_val[i];
+		cpr_write(cpr_vreg, offset, val);
+	}
 }
 
 static void cpr_corner_save(struct cpr_regulator *cpr_vreg, int corner)
@@ -923,6 +949,7 @@ static int cpr_suspend(struct cpr_regulator *cpr_vreg)
 	cpr_ctl_disable(cpr_vreg);
 
 	cpr_irq_clr(cpr_vreg);
+	cpr_regs_save(cpr_vreg);
 
 	cpr_vreg->is_cpr_suspended = true;
 
@@ -938,6 +965,7 @@ static int cpr_resume(struct cpr_regulator *cpr_vreg)
 	mutex_lock(&cpr_vreg->cpr_mutex);
 
 	cpr_vreg->is_cpr_suspended = false;
+	cpr_regs_restore(cpr_vreg);
 	cpr_irq_clr(cpr_vreg);
 
 	cpr_ctl_enable(cpr_vreg, cpr_vreg->corner);
@@ -1038,6 +1066,21 @@ static int __devinit cpr_config(struct cpr_regulator *cpr_vreg,
 	val |= RBCPR_CTL_TIMER_EN | RBCPR_CTL_COUNT_MODE;
 	val |= RBCPR_CTL_SW_AUTO_CONT_ACK_EN;
 	cpr_write(cpr_vreg, REG_RBCPR_CTL, val);
+
+	/* Registers to save & restore for suspend */
+	cpr_vreg->save_regs[0] = REG_RBCPR_TIMER_INTERVAL;
+	cpr_vreg->save_regs[1] = REG_RBCPR_STEP_QUOT;
+	cpr_vreg->save_regs[2] = REG_RBIF_TIMER_ADJUST;
+	cpr_vreg->save_regs[3] = REG_RBIF_LIMIT;
+	cpr_vreg->save_regs[4] = REG_RBIF_SW_VLEVEL;
+	cpr_vreg->save_regs[5] = REG_RBIF_IRQ_EN(cpr_vreg->irq_line);
+	cpr_vreg->save_regs[6] = REG_RBCPR_CTL;
+	cpr_vreg->save_regs[7] = REG_RBCPR_GCNT_TARGET
+		(cpr_vreg->cpr_fuse_ro_sel[CPR_FUSE_CORNER_SVS]);
+	cpr_vreg->save_regs[8] = REG_RBCPR_GCNT_TARGET
+		(cpr_vreg->cpr_fuse_ro_sel[CPR_FUSE_CORNER_NORMAL]);
+	cpr_vreg->save_regs[9] = REG_RBCPR_GCNT_TARGET
+		(cpr_vreg->cpr_fuse_ro_sel[CPR_FUSE_CORNER_TURBO]);
 
 	cpr_irq_set(cpr_vreg, CPR_INT_DEFAULT);
 
